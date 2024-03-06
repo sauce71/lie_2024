4d26da9a74e5415440a3971a5bacdd7b71cdde36
Tom Øyvind Hogstad <tom.oyvind.hogstad@gmail.com>
2024-03-06 21:17:13 +0100

    main.py med blink. Endret til å bare lese av TVOC i ags10 eksempel. Ga normale verdier.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import machine
 import time
 from ags10 import AGS10
 
+
+led = machine.Pin('LED', machine.Pin.OUT) # Setter pinne LED som en output pi
+
 # Setter opp i2c med en frekvens på 10khz
 i2c = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17), freq=10000)
 
@@ -10,8 +13,9 @@ sensor = AGS10(i2c) # Instans av sensoren
 
 
 while True:
+    led.toggle() # Slår led av og på
     tvoc = sensor.total_volatile_organic_compounds_ppb # Leser tvoc
-    #resistance = sensor.resistance_kohm # Leser motstand
-    print('TVOC:', tvoc) # Skriver ut den avleste verdien
+    print('TVOC:', tvoc) # Skriver ut de avleste verdiene
     time.sleep(10)
+    
     
